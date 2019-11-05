b2c4b8978ffa2a09f57017e2be09d1749715b972
abdi <abdivhhf@gmail.com>
2019-11-05 17:02:38 -0600

    added delete icon

--- student/src/components/StudentRow.vue
+++ student/src/components/StudentRow.vue
@@ -3,6 +3,9 @@
         <td>{{student.name}}</td>
         <td>{{student.starID}}</td>
         <td> <input type="checkbox" v-model="student.present" v-on:change="checked(student)"></td>
+        <td v-show="edit">
+            <img class="delete-icon" v-on:click="deleteStudent(student)" src="@/assets/delete.png">
+        </td>
     </tr>
 </template>
 
@@ -11,10 +14,16 @@ export default {
     name: 'StudentRow',
     props: {
         student:Object,
+        edit: Boolean
     },
     methods: {
         checked(student) {
             this.$emit('student-present', student)
+        },
+        deleteStudent(student) {
+            if (confirm(`Delete ${student.name}?`)) {
+                this.$emit('delete-student', student)
+            }
         }
     }
 }
